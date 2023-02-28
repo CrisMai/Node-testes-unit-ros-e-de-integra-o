73cf0f905974a7c0592a7b1e45646e73c308c34b
unknown <crismai.dev@gmail.com>
2023-02-28 14:23:54 -0300

    Asserções

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
 const calculaDescontos = (salario, descontos) => salario - descontos;
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        // eslint-disable-next-line no-throw-literal
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} Passou!`);
+  } catch {
+    console.error(`${titulo} Não passou!`);
   }
 };
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
+
+// eslint-disable-next-line quotes
+teste("somaHorasExtras", () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+// eslint-disable-next-line quotes
+teste("calculaDesconto", () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
--- index.md
+++ index.md
@@ -1,4 +1,4 @@
-Tipos de Testes
+----- Tipos de Testes -----
 
 Testes estáticos - voltados para analisar o código sem necessariamente executá-lo, 
 verificando se algumas boas práticas e formatação padronizada foram adotadas na implementação;
@@ -8,3 +8,11 @@ Testes unitários- são utilizados para verificar o comportamento das menores un
 Testes de integração - são as fases do teste de software em que módulos são combinados e testados como um conjunto;
 
 Teste E2E (End-to-End) - é um método de teste utilizado para testar um fluxo da aplicação desde o começo até o fim.
+
+
+------ Asserções ------
+
+Tipo específico de validação, checagem ou verificação. 
+As asserções poderão ser verdadeiras ou falsas.
+São expressões que verificamos e retornam um resultado. Mas elas não servem só para o tipo específico de números. 
+Elas podem funcionar com strings, com arrays, ou até com aqueles dados e objetos que nós mesmos criamos.
